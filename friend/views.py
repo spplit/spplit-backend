9d83ef183b6b6ffc3439be0074beabb84df6320e
Sanghyo <sanghyoboy@naver.com>
2021-07-23 17:36:11 +0900

    request create customize yet

--- friend/views.py
+++ friend/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from django.http.response import Http404
+from django.shortcuts import get_object_or_404
+from card.models import MyCard
 from .models import CardList, CardRequest
 from .serializers import CardListSerializers, CardRequestSerializers
 
@@ -20,10 +22,23 @@ class CardRequestViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(sender=self.request.user)
 
+    # send_friend_request 
+    def create(self, request, *args, **kwargs):
+        cardId = self.request.POST['cardId']
+        if cardId:
+            receiver = get_object_or_404(MyCard, id=cardId).author
+            card_request = CardRequest.objects.get(sender=self.user, receiver=receiver, cardId=cardId)
+            if card_request:
+                return Response(status=status.HTTP_409_CONFLICT)
+            added_card_request = CardRequest(sender=self.user, receiver=receiver, cardId=cardId)
+            added_card_request.save()
+
+    # accept > card_list에 추가 > request_list에서 제거
     @action(detail=False, method=['POST'])
     def accept(request):
         cardId = self.request.POST['cardId']
 
+    # decline > request_list에서 제거
     @action(detail=False, method=['POST'])
     def decline(request):
         cardId = self.request.POST['cardId']
